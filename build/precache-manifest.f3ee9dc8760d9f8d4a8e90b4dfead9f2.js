c0332a2a3c9a61e70f7dab9006b2120d1d9473d4
aleksanderbrymora <aleksander.australia@gmail.com>
2020-03-13 21:06:52 +1100

    added build folder

---
+++ build/precache-manifest.f3ee9dc8760d9f8d4a8e90b4dfead9f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce36fcdac067f14138272b236dc15f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1475be886ced440a117e",
+    "url": "/static/css/main.56ec71e8.chunk.css"
+  },
+  {
+    "revision": "948ed1be6efc3b56d84c",
+    "url": "/static/js/2.ba07bb3e.chunk.js"
+  },
+  {
+    "revision": "2d4e50db9bebc1822939159016933eb5",
+    "url": "/static/js/2.ba07bb3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1475be886ced440a117e",
+    "url": "/static/js/main.faccac67.chunk.js"
+  },
+  {
+    "revision": "cc73a51c2a764717df8c",
+    "url": "/static/js/runtime-main.d501b7ce.js"
+  }
+]);
